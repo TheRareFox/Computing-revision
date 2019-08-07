6fc741ad95a3a7d98686ace69950a6c5eb563248
Zhang Yuxiang <47800388+UwUUwU@users.noreply.github.com>
2019-08-07 08:57:09 +0800

    Update binary_search_iterative.py

--- Search and sort/binary_search_iterative.py
+++ Search and sort/binary_search_iterative.py
@@ -1,18 +1,16 @@
 from create_list import create_list_sorted
 
 def binary_search_iterative(arr,target):
-    half = int(len(arr)//2)
-    while target != arr[half]:
-        if half == 1:
+    half = len(arr)//2
+    while target is not arr[half]:
+        if len(arr) == 1:
             return "Not found"
-            break
         elif target>arr[half]:
             arr = arr[half:]
-            half = int(len(arr)//2)
+            half = len(arr)//2
         elif target <arr[half]:
             arr = arr[:half]
-            half = int(len(arr)//2)
-        
+            half = len(arr)//2
     return "Found {}".format(target)
 
 arr = create_list_sorted(1000)
