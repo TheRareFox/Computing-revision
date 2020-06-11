a7be8e720cf0231c3dd0f716444306b6c4952702
Zhang Yuxiang <47800388+UwUUwU@users.noreply.github.com>
2020-06-11 17:26:05 +0800

    Update insertion_sort.py

--- Search and sort/insertion_sort.py
+++ Search and sort/insertion_sort.py
@@ -9,7 +9,7 @@ def insertion_sort(arr):
         while j >= 0 and key < arr[j] : 
             arr[j + 1] = arr[j] 
             j -= 1
-            arr[j + 1] = key
+        arr[j + 1] = key
     return arr
 
 if __name__=="__main__":
